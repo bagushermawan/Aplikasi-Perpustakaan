5b1748c511bc433d2ef72f7dff936f69cd648819
bagusherma <bagusherma123@gmail.com>
2025-08-30 23:21:29 +0700

    add storage link at route web

--- be/routes/web.php
+++ be/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/foo', function () {
+    Artisan::call('storage:link');
+});
