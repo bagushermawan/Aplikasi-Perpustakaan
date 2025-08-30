f805bfddeedd8f444f68b813415866f3ef41262a
bagusherma <bagusherma123@gmail.com>
2025-08-31 00:41:59 +0700

    edit cors

--- be/config/cors.php
+++ be/config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://127.0.0.1:8000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://127.0.0.1:3000')],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
