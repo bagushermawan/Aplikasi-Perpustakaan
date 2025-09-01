667a07ea4c468201f4f5ab8745bfe0c5f915bbba
bagusherma <bagusherma123@gmail.com>
2025-09-01 18:27:23 +0700

    rollback route

--- be/routes/web.php
+++ be/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
@@ -13,7 +12,3 @@ require __DIR__.'/auth.php';
 Route::get('/foo', function () {
     Artisan::call('storage:link');
 });
-
-Route::post('/login', [AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest')
-    ->name('login');
