2d8b0a6ae562066fcf10c7bfc0e69aadad241f86
bagusherma <bagusherma123@gmail.com>
2025-09-02 09:26:38 +0700

    edit bootstrap/app, skip csrf token

--- be/bootstrap/app.php
+++ be/bootstrap/app.php
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
         $middleware->validateCsrfTokens(except: [
-            'login'
+            '*'
         ]);
 
         //
